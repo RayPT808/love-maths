4447b5e70af95cbd3a8b23d8904b8b07c9c53f12
RayPT808 <pitti.ray@gmail.com>
2024-04-30 14:32:03 +0000

    Fixing division code.

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
 
